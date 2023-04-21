1c792fb93dbfe8310b2d720a900be7afd5440ab6
OleksandrPinchuk <hohhua@gmail.com>
2023-04-21 02:19:02 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
-import Vimeo from "@vimeo/player";
+import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const TIME = "videoplayer-current-time";
 
-player.on('timeupdate', throttle(func, 1000));
+player.on('timeupdate', throttle(timePlayd, 1000));
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+function timePlayd(evt) {
+    localStorage.setItem(TIME, evt.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(TIME) || 0);
